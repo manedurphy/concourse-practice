bc3378e0290b668d35d3ec6c6e54efc9e7c704f0
Dane Murphy <danem7@gmail.com>
2022-02-18 14:00:54 -0800

    fifth commit

--- scripts/set_pipline.sh
+++ scripts/set_pipline.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+envsubst < templates/$CONFIG.tmpl > $CONFIG.yaml
+
 fly -t tutorial set-pipeline \
     --pipeline hello-world \
-    --config $CONFIG
+    --config $CONFIG.yaml
+
+rm $CONFIG.yaml
